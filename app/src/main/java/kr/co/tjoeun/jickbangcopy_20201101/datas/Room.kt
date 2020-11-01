54d8812945062cb4bae95e43bb5e777958b1f8d3
say-102 <say70007@naver.com>
2020-11-01 14:50:34 +0900

    Room에 기능 자체적으로 추가중  - 1억 이상 여부에 따라, 다른 양식의 가격(String) 리턴해주는 기능

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/datas/Room.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/datas/Room.kt
@@ -6,4 +6,21 @@ class Room(
     val floor: Int,
     val description: String) {
 
+//    부가기능 직접 제작
+//    1) price값에 따라서 가공된 가격 알려주기. (ex. 2억 8,500 or 3,600)
+
+    fun getFormmatedPrice() : String {
+
+//        price의 값이 1억(10000) 이상? 아닌가? 에 따라 다른 행동.
+
+        if (price >= 10000) {
+            return "1억 이상"
+        }
+        else {
+            return "1억이 안됨"
+        }
+    }
+
+//    2) 층수값에 따라서 몇층인지 알려주기. (ex. 2층, 반지하, 지하1층)
+
 }
